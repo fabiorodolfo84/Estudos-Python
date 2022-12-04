57407eb2662d579c8602d504a034e2dccb326a26
fabiorodolfo84 <fabiorodolfo1984@gmail.com>
2022-12-03 23:25:07 -0300

    update

--- Lista_bicycles.py
+++ Lista_bicycles.py
@@ -1,6 +1,6 @@
 #Acessar elementos individuais de uma lista.
 
-bicycles = ['Trek', 'Cannondale', 'redline','Specialized']
+bicycles = ['trek', 'cannondale', 'redline','specialized']
 print(bicycles)
 
 #Acessando elementos de uma lista
@@ -8,6 +8,16 @@ print(bicycles)
 print(bicycles[0])
 
 # o sistema ira imprimir o item [0] da lista que sera 'Cannondale'
+
+#tambem podemos usar os metodos de string em qualquer elemento de uma lista.
+#Por exemplo, podemos formatar o elemento 'trek de modo mais bonito 
+#usando o metodo tible().
+
+print(bicycles[0].title()) #este metodo gera a mesma saida do exemplo anterior
+#exceto pelo fato de 'Trek' começar com uma letra maiuscula
+
+
+
 
 
 
